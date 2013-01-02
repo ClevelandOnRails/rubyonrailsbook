fc8ab7a4277f82f21f22e50e54fd3963fbff325f
Pascal Precht <pascal.precht@googlemail.com>
2013-01-02 08:57:44 +0100

    Added html support. Yay.

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 html:
-	pandoc -s -N --section-divs index.md -t html5 -o index.html \
-		--css=stylesheets/book.css \
-		--include-in-header meta.html \
-		--include-before-body head.html \
-		--include-before-body preface.html \
-		--include-after-body footer.html \
+	pandoc -s -N --section-divs tmp/index.md -t html5 -o builds/html/index.html \
+		--css=main.min.css \
+		--include-in-header src/html/meta.html \
+		--include-before-body src/html/head.html \
+		--include-before-body src/html/preface.html \
+		--include-after-body src/html/footer.html \
 		--title-prefix '' \
 		--toc
--- grunt.js
+++ grunt.js
@@ -14,15 +14,50 @@ module.exports = function (grunt) {
       }
     },
 
+    copy: {
+
+      minified_css: {
+        files: {
+          "builds/html/": "src/css/main.min.css"
+        }
+      }
+    },
+
+    concat: {
+      markdown: {
+        src: 'src/chapters/*.md',
+        dest: 'tmp/index.md'
+      },
+
+      css: {
+        src: 'src/css/*.css',
+        dest: 'src/css/main.min.css'
+      }
+    },
+
+    mincss: {
+      main: {
+        files: {
+          'src/css/main.min.css': 'src/css/main.min.css'
+        }
+      }
+    },
+
     shell: {
       make_html: {
         command: 'make html'
       }
+    },
+
+    clean: {
+      tmp: 'tmp',
+      css: 'src/css/main.min.css'
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib');
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-sass');
 
-  grunt.registerTask('wbb:html', 'sass:compile');
+  grunt.registerTask('wbb:html', 'concat:markdown sass:compile concat:css mincss:main copy:minified_css shell:make_html clean:tmp clean:css');
 };
